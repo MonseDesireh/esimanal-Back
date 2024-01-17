613d2bb33b22e5c3817ce9ef9e4f9b591b5944e4
MonseDesireh <monserratdesirehf@gmail.com>
2024-01-16 18:33:17 -0600

    Arreglando modelos

--- main.py
+++ main.py
@@ -64,7 +64,11 @@ class User(db.Model):
             'nombre': self.nombre,
             'contacto': self.contacto,
             'titulo': self.titulo,
-            'role_id': self.role_id
+            'role_id': self.role_id,
+            'role': {
+                'id': self.role.id,
+                'descripcion': self.role.descripcion
+            }
         }
     
 
@@ -72,9 +76,11 @@ class Noticias(db.Model):
     __tablename__ = 'noticias'
     id = db.Column(db.Integer, primary_key = True)
     titulo = db.Column(db.String(50), nullable = False)
-    contenido = db.Column(db.String(50), nullable = False)
+    contenido = db.Column(db.String(255), nullable = False)
     categoria_id = db.Column(db.Integer, db.ForeignKey('cat_categorias.id'))
+    categoria = db.relationship('CatCategoria', backref='categoria', lazy=True)
     img_id = db.Column(db.Integer, db.ForeignKey('img_submissions.id'))
+    img = db.relationship('imgSubmissions', backref='img', lazy=True)
     autor_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     autor = db.relationship('User', backref='noticias_autor', lazy=True)
     created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable = False)
@@ -87,42 +93,53 @@ class Noticias(db.Model):
             'categoria_id': self.categoria_id,
             'img_id': self.img_id,
             'autor_id': self.autor_id,
-            'created_at': self.created_at
+            'created_at': self.created_at,
+            'categoria': {
+                'id': self.categoria.id,
+                'descripcion': self.categoria.descripcion
+            },
+            'autor': {
+                'id': self.autor.id,
+                'nombre': self.autor.nombre,
+                'contacto': self.autor.contacto,
+                'titulo': self.autor.titulo,
+                'role_id': self.autor.role_id,
+                'role': {
+                    'id': self.autor.role.id,
+                    'descripcion': self.autor.role.descripcion
+                }
+            },
         }
         
 with app.app_context():
     db.create_all()
 
-# @app.route('/post-news',  methods=['GET', 'POST'])
-# def postNews():
-#     try:
-#         data = request.get_json()
-#         cursor = mysql.connection.cursor()
-#         cursor.execute(f"""
-#                         INSERT INTO noticias (titulo, contenido, created_at, categoria_id, img_id, autor_id)
-#                         VALUES ('{data['titulo']}', '{data['contenido']}', '2024-01-01 00:00:00', 1, null, 1)
-#                         """)
+@app.route('/post-news',  methods=['GET', 'POST'])
+def postNews():
+    try:
+        data = request.get_json()
         
-#         mysql.connection.commit()
+        nueva_noticia = Noticias(
+            titulo = data['titulo'],
+            contenido = data['contenido'],
+            categoria_id = 1,
+            img_id = None,
+            autor_id = 1,
+            created_at = datetime.utcnow()
+        )
         
-#         return data
-#     except Exception as e:
-#         return jsonify({"message": f"Error de conexión: {str(e)}"})
-
-#     finally:
-#         cursor.close()
+        db.session.add(nueva_noticia)
+        db.session.commit()
+        
+        return data
+    except Exception as e:
+        return jsonify({"message": f"Error de conexión: {str(e)}"})
         
 @app.route('/get-news', methods=['GET'])
 def getNews():
     try:
-        result = (
-            db.session.query(Noticias)
-            .join(User, Noticias.autor_id == User.id)
-            .join(CatCategoria, Noticias.categoria_id == CatCategoria.id)
-            .options(joinedload(Noticias.autor).joinedload(User.role))
-            .all()
-        )
-
-        return jsonify([item.serialize() for item in result])
+        users = Noticias.query.options(db.joinedload(Noticias.autor)).all()
+        
+        return jsonify([user.serialize() for user in users])
     except Exception as e:
         return jsonify({"message": f"Error de conexión: {str(e)}"})
